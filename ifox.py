02af157b1822ba54e8a59f2e7bb00b2fe1db4753
Mahmood A. Jumaah <23147056+mwdx93@users.noreply.github.com>
2025-04-26 12:55:48 +0300

    Update ifox.py

--- ifox.py
+++ ifox.py
@@ -21,12 +21,14 @@
 # 
 # Mahmood A. Jumaah, Yossra H. Ali, Tarik A. Rashid, S. Vimal. 2024. FOXANN: 
 # A Method for Boosting Neural Network Performance. DOI: https://doi.org/10.48550/arXiv.2407.03369
+#
+# Mahmood A. Jumaah, Yossra H. Ali, Tarik A. Rashid, Efficient Q-learning hyperparameter tuning using FOX optimization algorithm,
+# Results in Engineering, Volume 25, 2025, 104341, ISSN 2590-1230, https://doi.org/10.1016/j.rineng.2025.104341.
 # -----------------------------------------------------------------------------
 
 
 import warnings
 import numpy as np
-from utils.helper_functions import cross_entropy_loss
 warnings.filterwarnings('ignore')
 
 class IFOX:
@@ -45,7 +47,12 @@ class IFOX:
         self.X_train = X_train
         self.y_train = y_train
 
-    
+    # Loss function
+    def cross_entropy_loss(self, y_true, y_pred):
+        n_samples = y_true.shape[0]
+        logp = - np.log(y_pred[np.arange(n_samples), y_true.argmax(axis=1)])
+        loss = np.sum(logp) / n_samples
+        return loss
     
     # Objective to be optimized
     def obj(self, sol):
